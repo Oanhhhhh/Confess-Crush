e6a01fc2755441b8d95204ab8b427816d5139cb3
Oanhhhhh <94666377+Oanhhhhh@users.noreply.github.com>
2021-11-19 16:21:48 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,20 +1,20 @@
 const CONFIG = {
-    titleWeb: "Tiêu đề web",
-    introTitle: 'Tên người yêu bạn',
+    titleWeb: "Web kiểm tra trapboiz",
+    introTitle: 'Hi , Nhân ',
     introDesc: `Trái đất vốn lạ thường
     Mà sao em cứ đi nhầm đường
     Lạc vào tim anh lẻ loi
     Đằng sau chữ yêu đây là thương`,
     btnIntro: '^^HiHi^^',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 🥰',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Thích lắm <33',
-    btnNo: 'Không nha :3',
-    question: 'Trên thế giới hơn 7 tỉ người mà sao bạn lại yêu mình <3',
-    btnReply: 'Gửi cho bạn <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Mình biết mà 🥰. Yêu bạn nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h, mình qua đón đi chơi nha.',
+    title: 'Phải chăng cậu là trapboiz đang lừa tớ 🥰',
+    desc: 'Trả lời thật lòng đii ',
+    btnYes: 'Nhân là trapboiz chính hiệu',
+    btnNo: 'Nhân chân thật như đất á',
+    question: 'Trên thế giới hơn 7 tỉ người mà sao cậu lại lừa tớ <3',
+    btnReply: 'Gửi cho cậu <3',
+    reply: 'vì Oanh siu cute phô mai que',
+    mess: 'Mình biết mà 🥰. Iu cậu nhiều nhiều 😘😘',
+    messDesc: 'Phắn đi nhá',
     btnAccept: 'Okiiiii lun <3',
     messLink: 'https://github.com/zukahai/Confess-Crush' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
